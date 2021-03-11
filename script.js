981c9e6ce01437886b919a371abe0599dd522558
markoscapin <59868532+markoscapin@users.noreply.github.com>
2021-03-11 18:21:03 +0100

    second and third step complete
    completed second and third step updating html and css without using js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Variables for sections on the page
+var game = document.querySelector("#game")
+var winOrLose = document.querySelector("#winOrLose")
+
+//ComputedStyle from sections on the page
+var gameStyle = getComputedStyle(game);
+var winOrLoseStyle = getComputedStyle(winOrLose);
 
 
 
